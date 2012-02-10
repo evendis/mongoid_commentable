607dca5549a47c53636712917e022bea01c0404b
Paul Gallagher <gallagher.paul@gmail.com>
2012-02-10 13:05:53 +0800

    add comments_list test and explicit requirement for Tiemstamps

--- lib/comment.rb
+++ lib/comment.rb
@@ -3,6 +3,7 @@ module Mongoid_Commentable
     extend ActiveSupport::Concern
 
     included do |base|
+      include Mongoid::Timestamps
       base.attr_accessible :path,:parent
       base.field :path, :type => String, :default => ""
       base.field :parent, :type => String
--- spec/mongoid/commentable_spec.rb
+++ spec/mongoid/commentable_spec.rb
@@ -4,17 +4,17 @@ describe Mongoid::Commentable do
   
   describe Comment do
    
-    context 'creating new comment' do 
+    context 'creating new comment' do
       subject {CommentableModel.new.create_comment!(:author => 'Author', :text => 'test') }
-      its(:class){should == Comment} 
-      its(:author){should == 'Author'} 
+      its(:class){should == Comment}
+      its(:author){should == 'Author'}
       its(:text){should == 'test'}
       specify{subject.path.should == "root.#{subject.id}"}
    end
 
     context 'replying to a comment' do
       before do
-        @commentable_model = CommentableModel.new  
+        @commentable_model = CommentableModel.new
         3.times{|i| @commentable_model.create_comment!(nil)}
         @parent = @commentable_model.comments.first
       end
@@ -26,7 +26,7 @@ describe Mongoid::Commentable do
     context 'marking comment as deleted' do
       before do
         @comment = CommentableModel.new.create_comment!(nil)
-        @comment.remove 
+        @comment.remove
       end
       subject {@comment}
       its(:deleted_at){should_not be_nil}
@@ -35,7 +35,7 @@ describe Mongoid::Commentable do
 
     context 'getting branch' do
       before do
-        @commentable_model = CommentableModel.new  
+        @commentable_model = CommentableModel.new
         @comment1 = @commentable_model.create_comment!(nil)
         @comment2 = @commentable_model.create_comment!(nil)
         @child1 = @commentable_model.create_comment!(:parent => @comment1.id.to_s)
@@ -43,6 +43,25 @@ describe Mongoid::Commentable do
       end
       subject{@commentable_model}
       specify{subject.branch_for(@comment1.id).should == [@comment1,@child1,@child2]}
+    end
+
+    describe "#comments_list" do
+      let(:commentable_model) { CommentableModel.new }
+      let!(:comment_a) { commentable_model.create_comment!(:author => 'Author', :text => 'comment_a') }
+      let!(:comment_b) { commentable_model.create_comment!(:author => 'Author', :text => 'comment_b') }
+      before do
+        comment_a.update_attribute(:created_at, 2.days.ago)
+      end
+      context "with default sort" do
+        subject { commentable_model.comments_list }
+        its(:first) { should eql(comment_a) }
+        its(:last) { should eql(comment_b) }
+      end
+      context "with :desc sort" do
+        subject { commentable_model.comments_list(:desc) }
+        its(:first) { should eql(comment_b) }
+        its(:last) { should eql(comment_a) }
+      end
     end
   end
 end
